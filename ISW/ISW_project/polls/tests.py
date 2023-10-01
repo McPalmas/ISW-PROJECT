27fb6ee48be592ed84ffa6e7d86e820086a56279
FedeGiugni <f.giugni99@gmail.com>
2023-10-01 18:59:47 +0200

    Aggiunto test unitario per la creazione di prodotto

--- ISW/ISW_project/polls/tests.py
+++ ISW/ISW_project/polls/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Prodotto
 
-# Create your tests here.
+
+#Unit test for Prodotto model
+
+class ProdottoModelTest(TestCase):
+    def setUp(self):
+        Prodotto.objects.create(id=100,nome='Prodotto di test', descrizione='Descrizione di test', prezzo=10.00)
+
+    def test_prodotto_creation(self):
+        prodotto = Prodotto.objects.get(id=100)
+        self.assertEqual(prodotto.nome, 'Prodotto di test')
+        self.assertEqual(prodotto.descrizione, 'Descrizione di test')
+        self.assertEqual(prodotto.prezzo, 10.00)
+    
+
