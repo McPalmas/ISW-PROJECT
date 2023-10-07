24244aedea805b63ae00b6eaabfd3d754a5fa02e
McPalmas <gianmarcopalmas32@gmail.com>
2023-10-07 14:18:49 +0200

    Aggiunta funzione di ricerca dei prodotti per l'admin e ordinamento prodotti in base a delle parole chiave

--- ISW/ISW_project/polls/admin.py
+++ ISW/ISW_project/polls/admin.py
@@ -4,4 +4,5 @@ from .models import Prodotto
 @admin.register(Prodotto)
 class ProdottoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'descrizione', 'prezzo')
-
+    list_filter = ["descrizione"]
+    search_fields = ["nome"]
