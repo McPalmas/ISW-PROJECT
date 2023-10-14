79b0629bf5fb4aa4b32fef13ae6af7126f8db862
McPalmas <gianmarcopalmas32@gmail.com>
2023-10-14 11:29:53 +0200

    Modifica rimozione, incremento e decremento prodotto
    Modifica sistema di incremento e decremento quantità di un prodotto nel carrello,  e aggiunta opzione per eliminare un prodotto completamente.

--- ISW/ISW_project/ISW_project/urls.py
+++ ISW/ISW_project/ISW_project/urls.py
@@ -34,7 +34,8 @@ urlpatterns = [
     path('', views.lista_prodotti, name="lista_prodotti"),
     path('carrello/', views.carrello, name="carrello"),
     path('aggiungi_al_carrello/<int:id>/', views.aggiungi_al_carrello, name="aggiungi_al_carrello"),
-    path('rimuovi_dal_carrello/<int:id>/', views.rimuovi_dal_carrello, name="rimuovi_dal_carrello"),
-    path('home_page/', views.home_page, name="home_page"),
+    path('remove_product/<int:id>/', views.remove_product, name="remove_product"),
+    path('increase_quantity/<int:id>/', views.increase_quantity, name="increase_quantity"),
+    path('decrease_quantity/<int:id>/', views.decrease_quantity, name="decrease_quantity"),
     path('pagamento/', views.CheckoutView.as_view(), name="pagamento")
 ]
--- ISW/ISW_project/polls/views.py
+++ ISW/ISW_project/polls/views.py
@@ -17,24 +17,22 @@ def lista_prodotti(request):
 
     prodotti = Prodotto.objects.all()
 
-    #recupero filtro e query di ordinamento
+    # recupero filtro e query di ordinamento
     order_by = request.GET.get('order_by')
     filtro_categoria = request.GET.get('filtro_categoria')
 
-    if order_by in ['prezzo','-prezzo','nome','-nome']:
+    if order_by in ['prezzo', '-prezzo', 'nome', '-nome']:
         prodotti = prodotti.order_by(order_by)
 
-
-    if filtro_categoria and filtro_categoria!='Tutte le categorie':
+    if filtro_categoria and filtro_categoria != 'Tutte le categorie':
         prodotti = prodotti.filter(categoria=filtro_categoria)
 
-
-    #recupero valore del form per la ricerca
+    # recupero valore del form per la ricerca
     search_term = request.GET.get('search')
     if search_term:
         prodotti = prodotti.filter(nome__icontains=search_term)
 
-    return render(request, 'polls/Prodotti.html', {'prodotti': prodotti,'categorie': categorie})
+    return render(request, 'polls/Prodotti.html', {'prodotti': prodotti, 'categorie': categorie})
 
 
 class UserLoginView(LoginView):
@@ -99,8 +97,20 @@ def aggiungi_al_carrello(request, id):
 
     return redirect('home')
 
+def remove_product(request, id):
+    product = Prodotto.objects.get(id=id)
+    carrello = []
 
-def rimuovi_dal_carrello(request, id):
+    if request.user.is_authenticated:
+        carrello, creato = Carrello.objects.get_or_create(user=request.user, completato=False)
+        elemento, creato = ElementoCarrello.objects.get_or_create(carrello=carrello, prodotto=product)
+        elemento.delete()
+
+    num_elementi_carrello = carrello.numero_elementi
+
+    return redirect('carrello')
+
+def decrease_quantity(request, id):
     product = Prodotto.objects.get(id=id)
     carrello = []
 
@@ -116,5 +126,15 @@ def rimuovi_dal_carrello(request, id):
     return redirect('carrello')
 
 
-def home_page(request):
-    return render(request, 'polls/Prodotti.html')
+def increase_quantity(request, id):
+    product = Prodotto.objects.get(id=id)
+    carrello = []
+
+    if request.user.is_authenticated:
+        carrello, creato = Carrello.objects.get_or_create(user=request.user, completato=False)
+        elemento, creato = ElementoCarrello.objects.get_or_create(carrello=carrello, prodotto=product)
+        elemento.quantita += 1
+        elemento.save()
+
+    return redirect('carrello')
+
