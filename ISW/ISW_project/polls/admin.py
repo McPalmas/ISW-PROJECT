f86fcfb68464b20d3501888331859abd90ec5336
McPalmas <gianmarcopalmas32@gmail.com>
2023-10-19 18:30:48 +0200

    Risolto problema ricerca e filtri prodotti dell'admin
    Aggiunta la ricerca e filtri anche per gli ordini/vendite

--- ISW/ISW_project/polls/admin.py
+++ ISW/ISW_project/polls/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([Carrello, ElementoCarrello, Prodotto, Ordine])
+admin.site.register([Carrello, ElementoCarrello ])
 
-
+@admin.register(Prodotto)
 class ProdottoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'descrizione', 'prezzo')
-    list_filter = ["descrizione"]
+    list_filter = ["descrizione","prezzo"]
     search_fields = ["nome"]
 
-
+@admin.register(Ordine)
 class OrdineAdmin(admin.ModelAdmin):
     list_display = ['user', 'nome', 'cognome', 'email', 'prezzo_complessivo_ordine']
     list_filter = ['user']
